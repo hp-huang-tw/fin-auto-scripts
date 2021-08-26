9ee88c8892c6fc324ce53df8138ccd10e69b1109
hp-huang-tw <stitchmos@gmail.com>
2021-08-26 16:53:19 +0800

    :hammer: refactor launch order

--- launch_order.py
+++ launch_order.py
@@ -8,24 +8,32 @@ from dealer.entrust import EnTrust
 from notify.line_notify import LineNotify
 
 
+class OrderLauncher:
+
+    def __init__(self):
+        load_dotenv()
+
+    def launch(self):
+        sinopac_password = os.getenv('SINOPAC_PASSWORD')
+        line_notify_token = os.getenv('LINE_NOTIFY_TOKEN')
+
+        line_notify_client = LineNotify(line_notify_token)
+
+        sinopac = SinoPac(sinopac_password)
+
+        if sinopac.launch_sfuture_trader():
+            line_notify_client.send_text("sfuture_trader is launched.")
+
+        sleep(1)
+        if sinopac.launch_eleader():
+            line_notify_client.send_text("eleader is launched.")
+
+        sleep(1)
+        entrust = EnTrust()
+        if entrust.launch_vip_trading_system():
+            line_notify_client.send_text("entrust vip is launched.")
+
+
 if __name__ == '__main__':
-    load_dotenv()
-
-    sinopac_password = os.getenv('SINOPAC_PASSWORD')
-    line_notify_token = os.getenv('LINE_NOTIFY_TOKEN')
-
-    line_notify_client = LineNotify(line_notify_token)
-
-    sinopac = SinoPac(sinopac_password)
-
-    if sinopac.launch_sfuture_trader():
-        line_notify_client.send_text("sfuture_trader is launched.")
-
-    sleep(1)
-    if sinopac.launch_eleader():
-        line_notify_client.send_text("eleader is launched.")
-
-    sleep(1)
-    entrust = EnTrust()
-    if entrust.launch_vip_trading_system():
-        line_notify_client.send_text("entrust vip is launched.")
+    launcher = OrderLauncher()
+    launcher.launch()
